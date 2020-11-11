c3a1aa2b9b1157029379dc605aa6e94567695fee
Mayur Barge <mayurbarge226@gmail.com>
2020-11-11 10:29:54 +0530

    Backtracing and io completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ChessPlayerTour
+The inputs to program are given with piece name followed by x and y coordinates in integer.
+e.g.
+Horse 0 0
+Output -
+Output is a 2D array providing steps taken by piece, each step has a step number
+e.g.
+0 5 6
+1 4 7
+2 3 8
+
+Left-Top corner is position 0,0 and right bottom is MAX, MAX
+
--- src/main/scala/game/io/Writer.scala
+++ src/main/scala/game/io/Writer.scala
@@ -15,31 +15,4 @@ object Writer {
     } yield (write(line))
   }
 
-  def writeResult(data: Option[Array[Array[Int]]]) = {
-    data match {
-      case Some(value) => {
-        printArray(value)
-        val resultString = value.map(row=>{
-          print("")
-          print(row.mkString("   "))
-        })
-      }
-      case None => print("No valid tour found")
-    }
-
-    def printArray(data: Array[Array[Int]]) = {
-      println("#############")
-      data.foreach(r => {
-        println()
-        print(r.mkString("  "))
-      })
-    }
-
-    def print(data: String) = {
-      for {
-        _ <- putStrLn(data)
-      } yield ()
-    }
-
-  }
 }
